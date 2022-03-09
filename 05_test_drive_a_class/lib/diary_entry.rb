8e92357ec32491d4f94d438375c0f86a084c627b
William Marshall <w.marshall@elsevier.com>
2022-03-09 13:37:36 +0000

    Wrote and passed rspec tests for diary_entry #reading_time and #reading_chunk

--- 05_test_drive_a_class/lib/diary_entry.rb
+++ 05_test_drive_a_class/lib/diary_entry.rb
@@ -1,7 +1,10 @@
 class DiaryEntry
+  attr_accessor :unread
+
   def initialize(title, contents) # title, contents are strings
     @title = title
     @contents = contents
+    @unread = @contents.split
   end
 
   def title
@@ -17,20 +20,33 @@ class DiaryEntry
     words.length
   end
 
-  def reading_time(wpm) # wpm is an integer representing the number of words the
-                        # user can read per minute
-    # Returns an integer representing an estimate of the reading time in minutes
-    # for the contents at the given wpm.
+  def reading_time(wpm)
+    word_count = self.count_words
+    if word_count % wpm == 0
+      reading_time = word_count / wpm
+    else
+      reading_time = word_count / wpm + 1
+    end
+    reading_time
   end
 
-  def reading_chunk(wpm, minutes) # `wpm` is an integer representing the number
-                                  # of words the user can read per minute
-                                  # `minutes` is an integer representing the
-                                  # number of minutes the user has to read
+  def reading_chunk(wpm, minutes)
+    return nil if wpm == 0 || minutes == 0
+    reading_chunk_count = wpm * minutes
+    reading_chunk = @unread[0..reading_chunk_count -1]
+    reading_chunk_count.times { @unread.shift }
+    @unread = @contents.split if @unread.empty?    
+    reading_chunk.join(" ")
+  end
+
+end
+
+    # `wpm` is an integer representing the number
+    # of words the user can read per minute
+    # `minutes` is an integer representing the
+    # number of minutes the user has to read
     # Returns a string with a chunk of the contents that the user could read
     # in the given number of minutes.
     # If called again, `reading_chunk` should return the next chunk, skipping
     # what has already been read, until the contents is fully read.
     # The next call after that it should restart from the beginning.
-  end
-end
--- 05_test_drive_a_class/spec/diary_entry_spec.rb
+++ 05_test_drive_a_class/spec/diary_entry_spec.rb
@@ -13,7 +13,18 @@ RSpec.describe DiaryEntry do
   end
 
   it "estimates reading time in minutes based on given wpm" do
+    expect(diary_entry.reading_time(1)).to eq(3)
+    expect(diary_entry.reading_time(2)).to eq(2)
     expect(diary_entry.reading_time(3)).to eq(1)
+    expect(diary_entry.reading_time(50)).to eq(1)
+  end
+
+  it "returns a string with a chunk of contents that the user could read in a given number of minutes, until contents are fully read" do
+    expect(diary_entry.reading_chunk(1, 1)).to eq("Test")
+    expect(diary_entry.reading_chunk(1, 1)).to eq("driving")
+    expect(diary_entry.reading_chunk(1, 1)).to eq("classes")
+    expect(diary_entry.reading_chunk(1, 1)).to eq("Test")
+    expect(diary_entry.reading_chunk(0, 0)).to eq(nil)
   end
 end
 
