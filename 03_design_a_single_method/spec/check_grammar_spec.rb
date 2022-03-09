80440db14c2ddc2b86a8d12849d16bfb6780d888
William Marshall <w.marshall@elsevier.com>
2022-03-09 10:38:58 +0000

    Refactored spec/check_grammar_spec.rb

--- 03_design_a_single_method/spec/check_grammar_spec.rb
+++ 03_design_a_single_method/spec/check_grammar_spec.rb
@@ -1,19 +1,25 @@
 require "check_grammar"
 
 RSpec.describe "#check_grammar" do
-  it "should return false if the string does not begin with a capital letter" do
-    expect(check_grammar("hello")).to eq(false)
-    expect(check_grammar("hello world.")).to eq(false)
+  context "when the string does not begin with a capital letter" do
+    it "should return false" do
+      expect(check_grammar("hello")).to eq(false)
+      expect(check_grammar("hello world.")).to eq(false)
+    end
   end
 
-  it "should return false if the string does not end with a suitable sentence-ending punctuation mark" do
-    expect(check_grammar("Hello world")).to eq(false)
-    expect(check_grammar("Hello world:")).to eq(false)
+  context "when the string does not end with a suitable sentence-ending punctuation mark" do
+    it "should return false" do
+      expect(check_grammar("Hello world")).to eq(false)
+      expect(check_grammar("Hello world:")).to eq(false)
+    end
   end
 
-  it "should return true if the string starts with a capital letter and ends with a suitable punctuation mark" do
-    expect(check_grammar("Hello, world.")).to eq(true)
-    expect(check_grammar("Hello, world!")).to eq(true)
-    expect(check_grammar("Hello, world?")).to eq(true)
+  context "when the string starts with a capital letter and ends with a suitable punctuation mark" do
+    it "should return true" do
+      expect(check_grammar("Hello, world.")).to eq(true)
+      expect(check_grammar("Hello, world!")).to eq(true)
+      expect(check_grammar("Hello, world?")).to eq(true)
+    end
   end
 end
